caaa2b3b35da60b49d7aa7f714f63663bb7e99d5
HrH_DeV <144415418+Harsh8323@users.noreply.github.com>
2025-05-11 00:52:36 +0530

    DOM elements Added

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const userNameInput = document.getElementById("username-input");
+  const searchBtn = document.getElementById("search-btn");
+  const errorMessage = document.getElementById("error-message");
+  const loading = document.getElementById("loading");
+  const profileCard = document.getElementById("profile-card");
+
+  // stats
+  const profilePic = document.getElementById("profile-pic");
+  const userNameText = document.getElementById("username-text");
+  const bio = document.getElementById("bio");
+  const tierBadge = document.getElementById("tier-badge");
+  const tierText = document.getElementById("tier-text");
+  const statsContainer = document.getElementById("stats-container");
+  const personalizedQuote = document.getElementById("personalized-quote");
+});
